0714d6cd47ec266071609f096c95a75183087c5f
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-04-10 16:14:30 +0200

    diagonal movement not working

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,20 +4,41 @@ function Player() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [frameIndex, setFrameIndex] = useState(0);
   const [activeRow, setActiveRow] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
   const [moving, setMoving] = useState(false);
+  const [arrowStates, setArrowStates] = useState({up: false, right: false, down: false, left: false});
 
   useEffect(() => {
     const handleKeyDown = (event) => {
       const speed = 10; // Adjust the speed as needed
+      
+
       switch (event.key) {
         case "ArrowUp":
-          setPosition((prevPosition) => ({
-            ...prevPosition,
-            y: prevPosition.y - speed,
-          }));
+          if(arrowStates.left === true) {
+            setPosition((prevPosition) => ({
+              x: prevPosition.x - speed,
+              y: prevPosition.y - speed,
+            }));
+          } else if (arrowStates.right === true) {
+            console.log("moving up right")
+            setPosition((prevPosition) => ({
+              x: prevPosition.x + speed,
+              y: prevPosition.y - speed,
+            }));
+          } else {
+            setPosition((prevPosition) => ({
+              ...prevPosition,
+              y: prevPosition.y - speed,
+            }));
+          }
           setActiveRow(2);
           setMoving(true);
+          if(arrowStates.up !== true) {
+          setArrowStates((previous) => ({
+            ...previous,
+            up: true
+          }));
+        }
           break;
         case "ArrowDown":
           setPosition((prevPosition) => ({
@@ -26,6 +47,12 @@ function Player() {
           }));
           setActiveRow(0);
           setMoving(true);
+          if(arrowStates.down !== true) {
+          setArrowStates((previous) => ({
+            ...previous,
+            down: true
+          }));
+        }
           break;
         case "ArrowLeft":
           setPosition((prevPosition) => ({
@@ -34,6 +61,12 @@ function Player() {
           }));
           setActiveRow(3);
           setMoving(true);
+          if(arrowStates.left !== true) {
+          setArrowStates((previous) => ({
+            ...previous,
+            left: true
+          }));
+        }
           break;
         case "ArrowRight":
           setPosition((prevPosition) => ({
@@ -42,6 +75,12 @@ function Player() {
           }));
           setActiveRow(1);
           setMoving(true);
+          if(arrowStates.right !== true) {
+          setArrowStates((previous) => ({
+            ...previous,
+            right: true
+          }));
+        }
           break;
         default:
           break;
@@ -49,8 +88,38 @@ function Player() {
     };
 
     const handleKeyUp = (event) => {
+      
+      switch (event.key) {
+        case "ArrowUp":
+          setArrowStates((previous) => ({
+            ...previous,
+            up: false
+          }));
+          break;
+        case "ArrowDown":
+          setArrowStates((previous) => ({
+            ...previous,
+            down: false
+          }));
+          break;
+        case "ArrowLeft":
+          setArrowStates((previous) => ({
+            ...previous,
+            left: false
+          }));
+          break;
+        case "ArrowRight":
+          setArrowStates((previous) => ({
+            ...previous,
+            right: false
+          }));
+          break;
+        default:
+          break;
+      }
         setMoving(false);
         setFrameIndex(0);
+        
     }
 
     window.addEventListener("keydown", handleKeyDown)
@@ -69,7 +138,6 @@ function Player() {
           setFrameIndex((prevIndex) => (prevIndex + 1) % 4); // Assuming 16 frames
       }
     }, 200); // Adjust the interval duration as needed
-    setIntervalId(id);
     return () => clearInterval(id);
   }, [moving]);
 
@@ -79,15 +147,13 @@ function Player() {
   const frameX = (frameIndex % framesPerRow) * spriteSize;
   const frameY = activeRow * spriteSize;
 
-  console.log(frameIndex)
-  const stopInterval = () => {
-    console.log("button clicked")
-   
+  const doSomething = () => {
+    console.log(arrowStates);
   }
 
   return (
     <div>
-    <button onClick={stopInterval}>Stop Interval</button>
+    <button onClick={doSomething}>Log Something</button>
     <div
       style={{ position: "absolute", top: position.y, left: position.x, backgroundPosition: `-${frameX}px -${frameY}px` }}
       className="container"

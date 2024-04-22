6a0c5147cdf7dfdb7cea5943c783714d49b56ec2
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-04-22 16:27:36 +0200

    starting npc conversation

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,60 @@ import MainScreen from "./pages/MainScreen";
 import StartScreen from "./pages/StartScreen";
 import GameOver from "./pages/GameOver";
 import Test from "./pages/Test";
-import IsPrivate from './components/IsPrivate';
+import IsPrivate from "./components/IsPrivate";
+import IsAnon from "./components/isAnon";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
-  
-  
-
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-       <Route  path="/" element={<IsPrivate><StartScreen /></IsPrivate>}/>
-       <Route  path="/game-over" element={<IsPrivate><GameOver /></IsPrivate>}/>
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              <Login />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <IsAnon>
+              <Register />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <IsPrivate>
+              <StartScreen />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/game-over"
+          element={
+            <IsPrivate>
+              <GameOver />
+            </IsPrivate>
+          }
+        />
 
-        <Route  path="/game" element={<IsPrivate><MainScreen /></IsPrivate>}/>
-        <Route  path="test" element={<Test />}/>
-
+        <Route
+          path="/game"
+          element={
+            <IsPrivate>
+              <MainScreen />
+            </IsPrivate>
+          }
+        />
+        <Route path="test" element={<Test />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/maps/GreenLand.jsx
+++ src/maps/GreenLand.jsx
@@ -2,31 +2,53 @@ import Player from "../components/Player";
 import { useState, useEffect, useRef, useContext } from "react";
 import { LevelContext } from "../context/level.context";
 
-function GreenLand({ backgroundPosition, position, frameX, frameY, conversation }) {
-  const {
-    greenLandInitialObjectPositions,
-    greenLandObjectCenterPositions,
-    textBox,
-  } = useContext(LevelContext);
+function GreenLand({
+  backgroundPosition,
+  position,
+  frameX,
+  frameY,
+  conversation,
+}) {
+  const { greenLandInitialObjectPositions, greenLandObjectCenterPositions } =
+    useContext(LevelContext);
+  const [playerInput, setPlayerInput] = useState("");
+  // conversation variables
+  let index = 0;
+  const speed = 100; // Adjust typing speed here
+  const npcElemment = document.getElementById("typing-text"); // DOM Element for displaying what the NPC is saying
+  const playerElement = document.getElementById("player-text");
+  let npxText;
 
-  function typeWriter(text) {
-    let index = 0;
-    const speed = 100; // Adjust typing speed here
-
-    if (index < text.length) {
-      document.getElementById("typing-text").innerHTML += text.charAt(index);
+  function typeWriter() {
+    if (index < npxText.length) {
+      npcElemment.innerHTML += npxText.charAt(index);
       index++;
       setTimeout(typeWriter, speed);
+    } else{
+      return "done";
     }
   }
+  
+
 
   const startConversation = async () => {
     try {
       const response = await fetch("http://localhost:5005/conversation", {
         method: "GET",
       });
-      console.log(response);
-      typeWriter(response.json());
+      const parsedText = await response.json();
+      npxText = parsedText.message;
+      npcElemment.style.backgroundColor = "rgb(150, 150, 150)";
+      npcElemment.style.padding = "10px";
+      npcElemment.style.borderRadius = "20px";
+      playerElement.style.display = "block";
+      typeWriter();
+      const npcDone = await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("done");
+        }, speed*npxText.length);
+      });
+      console.log(npcDone)
     } catch (error) {
       console.log(error);
     }
@@ -34,8 +56,7 @@ function GreenLand({ backgroundPosition, position, frameX, frameY, conversation 
 
   useEffect(() => {
     if (conversation === true) {
-      //conversation();
-      console.log("start conversation")
+      startConversation();
     }
   }, [conversation]);
   // update positions of objects when screen srolls
@@ -71,6 +92,9 @@ function GreenLand({ backgroundPosition, position, frameX, frameY, conversation 
       ></div>
       <div style={{ position: "absolute", top: 500, left: 900 }}>
         <p id="typing-text"></p>
+      </div>
+      <div style={{ position: "absolute", top: position.y - 50, left: position.x, borderRadius: "20px", padding:"10px" }}>
+        <input id="player-text" type="text" style={{display:"none"}} value={playerInput} onChange={(e) => setPlayerInput(e.target.value)}></input>
       </div>
     </div>
   );
